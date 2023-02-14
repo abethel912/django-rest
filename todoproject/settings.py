7874c566311f126aa0dccdcd017eeead65d8c04f
abethel912 <abethel180@gmail.com>
2023-02-14 18:56:03 -0500

    Settings.pt created

---
+++ todoproject/settings.py
@@ -0,0 +1,12 @@
+import dj_database_url
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "todos.apps.TodosConfig",
+    "rest_framework"
+]
